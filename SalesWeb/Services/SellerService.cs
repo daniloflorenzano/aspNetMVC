c37d621c983912a467ab0f9ca299af6c26c0a6e9
daniloflorenzano <danilo@directdigital.com.br>
2022-07-06 14:35:01 -0300

    Implementa funcao de Update no vendedor

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,8 +1,9 @@
-﻿using SalesWeb.Data;
-using SalesWeb.Models;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
+using SalesWeb.Data;
+using SalesWeb.Models;
+using SalesWeb.Services.Exceptions;
 
 namespace SalesWeb.Services
 {
@@ -36,6 +37,23 @@ namespace SalesWeb.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+                throw new NotFoundException("Id not found");
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException error)
+            {
+                throw new DbConcurrencyException(error.Message);
+            }
+
         }
     }
 }
